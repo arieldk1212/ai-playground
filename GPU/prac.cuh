fea8e1005e710918aab8c7f1247aedc11a24f982
arieldk1212 <arieldk6@gmail.com>
2026-05-15 18:16:19 +0300

    Created new prac.cuh file in GPU

---
+++ GPU/prac.cuh
@@ -0,0 +1,5 @@
+__global__ void naive(int M, int N, int K, float alpha, const float* A,
+                      const float* B, float beta, float* C) {
+  // https://www.youtube.com/watch?v=_4wFDby12ng&list=PLuOtX1Aplgctr3pQHOzErcEuOD35FT4tB&index=2
+  // @ 8:33
+}
