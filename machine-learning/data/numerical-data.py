54baf18a9a4bb96d68ea853973c46d528e5699c7
arieldk1212 <arieldk6@gmail.com>
2026-05-19 19:45:02 +0300

    restructure, data section now

---
+++ machine-learning/data/numerical-data.py
@@ -0,0 +1,5 @@
+"""
+Integers or floatin-point values that behave like numbers.
+Temperature, Weight, The number of deer wintering in a nature preserve
+BUT number like postal code belongs to categorial data, no relation when you multiply for example 2002 postal code to the 4004 postal code.
+"""
